2ce8802c24ae8babdc38db66c20e21496dff9996
Lindsay Barrett <labarrett22@gmail.com>
2016-06-07 08:10:22 -0700

    new

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,10 @@
 class Pin < ActiveRecord::Base
   validates_presence_of :description, :pin_image
-  belongs_to :user
 
+  belongs_to :user
 
   has_attached_file :pin_image, styles: { medium: "300x300>", thumb: "100x100>" }
     validates_attachment_content_type :pin_image, content_type: /\Aimage\/.*\Z/
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
 has_many :pins
 
+
+has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_presence_of :username
 
 end
